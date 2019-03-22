f94e5d5c8e5b44de8d4b4ca718f9a5385f79fab7
yuhao lan <yuhaolan@gatech.edu>
2019-03-22 19:05:39 -0400

    test rescheduling

--- core/master-client.cpp
+++ core/master-client.cpp
@@ -25,12 +25,25 @@ using masterworker::Worker;
 pthread_t tid1[NUM_CHUNK]; 
 
 
+
 struct thread_data {
 
    string machineip;
    string filename;
 
 };
+
+std::vector<thread_data> redomapping;
+string MACHINEONE = "myVMDeployed3:50051";
+string MACHINETWO = "myVMDeployed4:50051";
+string MACHINETHREE = "myVMDeployed5:50051";
+boolean MACHINEONEOK = true;
+boolean MACHINETWOOK = true;
+boolean MACHINETHREEOK = true;
+
+
+
+
 
 class MasterClient {
  public:
@@ -49,10 +62,10 @@ class MasterClient {
 
     // Context for the client. It could be used to convey extra information to
     // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
-    //set the timer
-    std::chrono::steady_clock::time_point deadline = std::chrono::system_clock::now() + std::chrono::milliseconds(100);
-    context.set_deadline(deadline);
+    // ClientContext context;
+    // //set the timer
+    // std::chrono::steady_clock::time_point deadline = std::chrono::system_clock::now() + std::chrono::milliseconds(100);
+    // context.set_deadline(deadline);
 
     // The actual RPC.
     Status status = stub_->StartMapper(&context, filename, &return_filename);
@@ -62,10 +75,12 @@ class MasterClient {
       return return_filename.filename();
     } else {
       //status is not ok
+
       std::cout << status.error_code() << ": " << status.error_message() << std::endl;
       return "RPC failed";
     }
   }
+
  std::string StartReducer(const std::string& str_filenames) {
   
     Filenames filenames;
@@ -95,7 +110,23 @@ void* startmapper(void *arg) {
     std::string input_filename(my_data->filename);
     std::string output_filename = cli.StartMapper(input_filename);
     //RPC failed or splitblob.499.map 
+    //store the failer results
     //TODO add the timer
+
+    if(output_filename == "RPC failed") {
+
+      if(my_data->machineip == MACHINEONE)
+        MACHINEONEOK = false;
+
+      if(my_data->machineip == MACHINETWO)
+        MACHINETWOOK = false;
+
+      if(my_data->machineip == MACHINETHREE)
+        MACHINETHREEOK = false;
+
+      redomapping.push_back({my_data->machineip, input_filename});
+
+    }
 
 
     std::cout << "Worker received: " << output_filename << std::endl;
@@ -108,7 +139,7 @@ void* startmapper(void *arg) {
 
 void startreducer() { 
 
-  MasterClient cli(grpc::CreateChannel("myVMDeployed3:50051", grpc::InsecureChannelCredentials()));
+  MasterClient cli(grpc::CreateChannel(MACHINEONE, grpc::InsecureChannelCredentials()));
   std::string input_filenames("mapresults/");
   std::string output_filename = cli.StartReducer(input_filenames);
   std::cout << "Worker-reducer received: " << output_filename << std::endl;  
@@ -151,15 +182,15 @@ int main(int argc, char** argv) {
 
       if(i == 0 || i % 3 == 0) {
 
-         td1[i].machineip = "myVMDeployed3:50051";
+         td1[i].machineip = MACHINEONE;
 
       } else if(i % 3 == 2) {
 
-         td1[i].machineip = "myVMDeployed4:50051";
+         td1[i].machineip = MACHINETWO;
 
       } else if(i % 3 == 1) {
 
-         td1[i].machineip = "myVMDeployed5:50051";
+         td1[i].machineip = MACHINETHREE;
       }
 
     }
@@ -179,6 +210,65 @@ int main(int argc, char** argv) {
       pthread_join(tid1[i], NULL); 
 
     }
+
+
+
+    //check which part needs redo:
+
+    int REMAP_SIZE = redomapping.size();
+
+    if(REMAP_SIZE != 0) {
+
+      pthread_t tid2[REMAP_SIZE]; 
+
+
+      count << "Rescheduling the failed worker tasks..." << endl;
+
+      int count = 0;
+
+      struct thread_data td2[REMAP_SIZE];
+
+      for(int i = 0; i < redomapping.size(); i++) {
+
+        if(MACHINEONEOK) {
+
+          count << "Rescheduling to Machine: " << MACHINEONE << endl;
+
+          td2[i].machineip = MACHINEONE;
+
+        } else if(MACHINETWOOK) {
+
+          count << "Rescheduling to Machine: " << MACHINETWO << endl;
+
+          td2[i].machineip = MACHINETWO;
+
+        } else {
+
+          count << "Rescheduling to Machine: " << MACHINETHREE << endl;
+
+          td2[i].machineip = MACHINETHREE;
+
+        }
+        td2[i].filename = redomapping[i].filename;
+      }
+    }
+
+     for(int i = 0; i < REMAP_SIZE; i++) { 
+
+        error = pthread_create(&(tid2[i]), NULL, &startmapper, (void*) &td2[i]); 
+
+        if (error != 0) 
+            printf("\nThread can't be created :[%s]", strerror(error)); 
+       
+    } 
+   
+    for(int i = 0; i < REMAP_SIZE; i++) {
+
+      pthread_join(tid2[i], NULL); 
+
+    }
+
+
 
     startreducer();
 
